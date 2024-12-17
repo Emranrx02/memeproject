4e4d842ffbfd39ab990f9d676efdc92c4b6337f5
shakil1886 <shakil@trynota.ai>
2024-12-17 20:27:23 +0600

    added

--- app/components/NavBar/NavBar.tsx
+++ app/components/NavBar/NavBar.tsx
@@ -2,11 +2,12 @@ import Image from "next/image";
 import MainLayout from "../utils/MainLayout";
 import { FaTelegramPlane } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
+import Link from "next/link";
 
 export default function NavBar() {
   const navItem = [
-    { label: <FaTelegramPlane size={32} />, slug: "www.telegram.com" },
-    { label: <FaXTwitter size={32} />, slug: "www.twitter.com" },
+    { label: <FaTelegramPlane size={32} />, slug: "https://t.me/BasePengu" },
+    { label: <FaXTwitter size={32} />, slug: "https://x.com/PenguBased" },
   ];
   return (
     <nav className="w-full fixed z-50 py-3 border-b  bg-[#0164FF]">
@@ -21,11 +22,15 @@ export default function NavBar() {
           />
           <div className="flex text-white gap-x-2 items-center">
             {navItem.map((item) => (
-              <div key={item.slug}>{item.label}</div>
+              <Link href={item.slug} target="_blank" key={item.slug}>
+                <div key={item.slug}>{item.label}</div>
+              </Link>
             ))}
-            <button className="text-xl uppercase ml-3 font-bold rounded-full bg-white text-[#0164FF] px-5 py-2">
-              buy now
-            </button>
+            <Link target="_blank" href={"#"}>
+              <button className="text-xl uppercase ml-3 font-bold rounded-full bg-white text-[#0164FF] px-5 py-2">
+                buy now
+              </button>
+            </Link>
           </div>
         </div>
       </MainLayout>
