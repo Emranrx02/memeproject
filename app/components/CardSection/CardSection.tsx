47df4e815b042d678bcbb34e6f2780478c28d393
shakil1886 <shakil@trynota.ai>
2024-12-17 20:04:14 +0600

    added

--- app/components/CardSection/CardSection.tsx
+++ app/components/CardSection/CardSection.tsx
@@ -26,9 +26,9 @@ export default function CardSection() {
       <MainLayout>
         <div className="mx-10">
           <div className="grid border-b-4 border-t-4 border-white  py-10 grid-cols-3 gap-x-3 sm:gap-x-7 gap-y-10">
-            <VerticleSlider images={images1} speed={300} />
-            <VerticleSlider images={images2} speed={600} />
-            <VerticleSlider images={images3} speed={500} />
+            <VerticleSlider images={images1} speed={250} />
+            <VerticleSlider images={images2} speed={400} />
+            <VerticleSlider images={images3} speed={250} />
           </div>
         </div>
         <div className="my-20 text-center max-w-[45rem] text-white  mx-auto">
--- app/components/HeaderSection/HeaderSection.tsx
+++ app/components/HeaderSection/HeaderSection.tsx
@@ -4,43 +4,50 @@ import MainLayout from "../utils/MainLayout";
 export default function HeaderSection() {
   return (
     <section className=" text-center  ">
-      <div className=" pt-56 ">
+      <div className=" pt-32 ">
         <MainLayout>
           <div className="max-w-[60rem] mx-auto">
-            <h1 className="text-5xl sm:text-8xl  inline-block relative customtext font-extrabold text-white">
-              BASE
+            <div>
+              <h1 className="text-5xl uppercase sm:text-8xl  inline-block relative customtext font-extrabold text-white">
+                Base
+              </h1>
+              <h1 className="text-5xl sm:text-8xl mr-9  inline-block relative customtext-d font-extrabold text-white">
+                <span className="inline-block">
+                  <Image
+                    src={"/base.png"}
+                    alt="base"
+                    className=" w-5 sm:w-9 right-0 absolute"
+                    width={1920}
+                    height={1080}
+                  />
+                  D
+                </span>
+              </h1>
+
+              <h1 className="text-5xl sm:text-8xl  inline-block relative customtext-pen font-extrabold text-white">
+                PEN
+              </h1>
+              <h1 className="text-5xl sm:text-8xl  inline-block relative customtext-g font-extrabold text-white">
+                <span className="inline-block">
+                  <Image
+                    src={"/mounth.png"}
+                    alt="base"
+                    className="w-5 sm:w-9 right-0 top-[50%] translate-y-[-50%] absolute"
+                    width={1920}
+                    height={1080}
+                  />
+                  G
+                </span>
+              </h1>
+              <h1 className="text-5xl sm:text-8xl  inline-block relative customtext-uins font-extrabold text-white">
+                U
+              </h1>
+            </div>
+            <h1 className="text-5xl uppercase sm:text-7xl inline-block customtext-bengu  relative  mt-10 font-extrabold text-white">
+              $Bengu
             </h1>
-            <h1 className="text-5xl sm:text-8xl  inline-block relative customtext-d font-extrabold text-white">
-              <span className="inline-block">
-                <Image
-                  src={"/base.png"}
-                  alt="base"
-                  className=" w-5 sm:w-9 right-0 absolute"
-                  width={1920}
-                  height={1080}
-                />
-                D
-              </span>
-            </h1>
-            <h1 className="text-5xl sm:text-8xl  inline-block relative customtext-pen font-extrabold text-white">
-              PEN
-            </h1>
-            <h1 className="text-5xl sm:text-8xl  inline-block relative customtext-g font-extrabold text-white">
-              <span className="inline-block">
-                <Image
-                  src={"/mounth.png"}
-                  alt="base"
-                  className="w-5 sm:w-9 right-0 top-[50%] translate-y-[-50%] absolute"
-                  width={1920}
-                  height={1080}
-                />
-                G
-              </span>
-            </h1>
-            <h1 className="text-5xl sm:text-8xl  inline-block relative customtext-uins font-extrabold text-white">
-              UINS
-            </h1>
-            <p className="text-white text-lg font-semibold max-w-[40rem] mx-auto mt-10">
+
+            <p className="text-white  text-lg font-semibold max-w-[40rem] mx-auto mt-20">
               BASED PENGUINS ARE 5,000 UNIQUE COLLECTIBLE PENGUINS WITH PROOF OF
               OWNERSHIP STORED ON THE BASE BLOCKCHAIN.
             </p>
